9530790fe618b88ff60a1ac09273ef8f8a7a67a0
gulshan_mphrx <gahluwalia@mphrx.com>
2020-02-07 02:23:06 +0530

    Unit and UI Tests added

---
+++ app/src/main/java/com/example/myapplication/ui/weather/data/NetworkWeatherRepository.kt
@@ -0,0 +1,60 @@
+package com.example.myapplication.ui.weather.data
+
+import com.example.myapplication.network.ErrorResponseModel
+import com.example.myapplication.network.NetworkException
+import com.example.myapplication.network.WeatherNetworkService
+import com.example.myapplication.network.await
+import com.example.myapplication.ui.weather.data.model.CurrentWeatherResponseModel
+import com.example.myapplication.ui.weather.data.model.WeatherForecastResponseModel
+import retrofit2.Call
+import retrofit2.Retrofit
+
+/**
+ * Created by Gulshan Ahluwalia on 2020-02-04.
+ */
+class NetworkWeatherRepository(val retrofit: Retrofit) : WeatherRepository {
+    private val retrofitService: WeatherNetworkService by lazy {
+        retrofit.create(WeatherNetworkService::class.java)
+    }
+
+    @Throws(RepositoryException.CurrentWeatherException::class)
+    override suspend fun fetchCurrentWeather(cityName: String): CurrentWeatherResponseModel? {
+        return launchAPI({
+            return@launchAPI retrofitService.getCurrentWeather(cityName)
+        }, { e -> RepositoryException.CurrentWeatherException(e.errorResponseModel) })
+    }
+
+    @Throws(RepositoryException.WeatherForecastException::class)
+    override suspend fun fetchWeatherForecast(
+        latitude: Double,
+        longitude: Double
+    ): WeatherForecastResponseModel? {
+        return launchAPI({
+            return@launchAPI retrofitService.getForecastWeather(latitude, longitude)
+        }, { e -> RepositoryException.CurrentWeatherException(e.errorResponseModel) })
+    }
+
+
+    private suspend fun <T : Any> launchAPI(
+        apiCall: () -> Call<T>,
+        exception: (e: NetworkException) -> RepositoryException
+    ): T? {
+        try {
+            return apiCall().await()
+        } catch (e: NetworkException) {
+            throw exception(e)
+        }
+    }
+
+    sealed class RepositoryException(val errorResponseModel: ErrorResponseModel) :
+        Throwable(errorResponseModel.message) {
+        class CurrentWeatherException(errorResponseModel: ErrorResponseModel) :
+            RepositoryException(errorResponseModel)
+
+        class WeatherForecastException(errorResponseModel: ErrorResponseModel) :
+            RepositoryException(errorResponseModel)
+
+        class GeneralException(errorResponseModel: ErrorResponseModel) :
+            RepositoryException(errorResponseModel)
+    }
+}
