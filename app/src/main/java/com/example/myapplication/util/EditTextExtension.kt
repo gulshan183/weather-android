17952b7201111be175ab284bc9cd5b369e51d6d1
gulshan_mphrx <gahluwalia@mphrx.com>
2020-02-07 20:43:20 +0530

    Keyboard issue fixed

--- app/src/main/java/com/example/myapplication/util/EditTextExtension.kt
+++ app/src/main/java/com/example/myapplication/util/EditTextExtension.kt
@@ -1,20 +1,29 @@
 package com.example.myapplication.util
 
+import android.app.Activity
+import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+
 
 /**
  * Created by Gulshan Ahluwalia on 2020-02-05.
  */
 
 fun EditText.onSubmit(func: () -> Unit) {
-    setOnEditorActionListener { _, actionId, _ ->
 
+    setOnEditorActionListener { _, actionId, _ ->
         if (actionId == EditorInfo.IME_ACTION_SEARCH) {
             func()
-        }
-
-        true
-
+            hideKeyboardFrom(this)
+            false
+        } else false
     }
+}
+
+fun hideKeyboardFrom(view: View) {
+    val imm: InputMethodManager =
+        view.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
